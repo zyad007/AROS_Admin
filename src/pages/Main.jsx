d0489834a0b23b77577ea1f5f924a8b986c4cc89
Zyad Abdul-Nasser <zizo_007@live.com>
2024-02-05 16:24:46 +0200

    added-react-map-gl

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Map, { Marker, Popup } from 'react-map-gl/maplibre'
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const DEMOTILES_MAPLIBRE = "https://demotiles.maplibre.org/style.json"
+const MAPTILER = 'https://api.maptiler.com/maps/streets/style.json?key=ZFtcxc8UpIJArb56xQlZ'
 
 export default function Main() {
   return (
-    <div>Map</div>
+    <div className='h-full w-full' >
+      <Map
+        initialViewState={{
+          latitude: 26.9,
+          longitude: 30.4,
+          zoom: 5.5
+        }}
+        mapStyle={MAPTILER}
+        attributionControl={false}
+      >
+        <Marker longitude={30.4} latitude={26.9} anchor="bottom"
+          draggable={true}
+        >
+
+        </Marker>
+
+      </Map>
+    </div>
   )
 }
