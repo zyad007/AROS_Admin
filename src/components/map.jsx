3b458982b681b1af5bb24f18aa2e83a2f55c2a4b
Bichoo <bichoyatef0@gmail.com>
2024-02-23 21:18:57 +0200

    bichoo

---
+++ src/components/map.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import MapGL, { Marker, Popup } from 'react-map-gl/maplibre';
+
+const MAPTILER = 'https://api.maptiler.com/maps/streets/style.json?key=ZFtcxc8UpIJArb56xQlZ';
+
+export default function Map({ obstacles, setPopupInfo, popupInfo }) {
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    if (map) {
+      try {
+        map.on('load', function () {
+          map.addSource('egypt_cities', {
+            type: 'geojson',
+            data: '../../public/metadata-egy_admbnda_adm1_capmas_20170421-zip.geojson'
+          });
+  
+          map.addLayer({
+            'id': 'egypt_cities',
+            'type': 'fill',
+            'source': 'egypt_cities',
+            'layout': {},
+            'paint': {
+              'fill-color': '#088',
+              'fill-opacity': 0.8
+            }
+          });
+  
+          map.addLayer({
+            'id': 'outline',
+            'type': 'line',
+            'source': 'egypt_cities',
+            'layout': {},
+            'paint': {
+              'line-color': '#888',
+              'line-width': 1
+            }
+          });
+        });
+      }  catch (error) {
+        console.error('Error loading GeoJSON data:', error);
+      }
+    }
+  }, [map]);
+
+  return (
+    <MapGL
+      width='100%'
+      height='100%'
+      initialViewState={{
+        latitude: 26.9,
+        longitude: 30.4,
+        zoom: 5.5
+      }}
+      mapStyle={MAPTILER}
+      attributionControl={false}
+      onViewportChange={viewport => setMap(viewport)}
+    >
+      {obstacles.map((obstacle, index) => (
+        <Marker key={index} latitude={obstacle.lat} longitude={obstacle.lon}>
+          <img 
+            src={`Road icons/${obstacle.type}.png`} 
+            alt={obstacle.type} 
+            className='w-6 h-6 rounded-full'
+            onMouseEnter={() => setPopupInfo(obstacle)}
+            onMouseLeave={() => setPopupInfo(null)}
+          />
+        </Marker>
+      ))}
+
+      {popupInfo && (
+        <Popup
+          latitude={popupInfo.lat}
+          longitude={popupInfo.lon}
+          onClose={() => setPopupInfo(null)}
+        >
+          <div>
+            <h3>{popupInfo.type}</h3>
+            <img src={popupInfo.imageURL} alt={popupInfo.type} className='w-32 h-auto' />
+            <p>{popupInfo.description}</p>
+          </div>
+        </Popup>
+      )}
+    </MapGL>
+  );
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
-import Map, { Marker, Popup } from 'react-map-gl/maplibre';
+import Map from '../components/map';
 import 'maplibre-gl/dist/maplibre-gl.css';
-
-const MAPTILER = 'https://api.maptiler.com/maps/streets/style.json?key=ZFtcxc8UpIJArb56xQlZ';
 
 export default function Main() {
   const [popupInfo, setPopupInfo] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(''); // Initialize to an empty string
 
   const obstacles = [
-    { 
-      lat: 26.9, 
+    {
+      lat: 26.9,
       lon: 30.4,
       type: 'accident',
       imageURL: 'accident1.jpeg',
-      description: 'Accident occurred here' 
-    }, { 
-      lat: 27.9, 
-      lon: 31.4, 
+      description: 'Accident occurred here'
+    }, {
+      lat: 27.9,
+      lon: 31.4,
       type: 'accident',
       imageURL: 'accident2.jpg',
-      description: 'Accident occurred here' 
+      description: 'Accident occurred here'
     }, {
       lat: 28.9,
       lon: 32.4,
@@ -41,45 +40,25 @@ export default function Main() {
     }
   ];
 
-  return (
-    <div className='h-full w-full' >
-      <Map
-        width='100%'
-        height='100%'
-        initialViewState={{
-          latitude: 26.9,
-          longitude: 30.4,
-          zoom: 5.5
-        }}
-        mapStyle={MAPTILER}
-        attributionControl={false}
-      >
-        {obstacles.map((obstacle, index) => (
-          <Marker key={index} latitude={obstacle.lat} longitude={obstacle.lon}>
-            <img 
-              src={`Road icons/${obstacle.type}.png`} 
-              alt={obstacle.type} 
-              className='w-6 h-6 rounded-full'
-              onMouseEnter={() => setPopupInfo(obstacle)}
-              onMouseLeave={() => setPopupInfo(null)}
-            />
-          </Marker>
-        ))}
+  const egyptCities = ['cairo', 'alex', 'sharm', 'gouna'];
 
-        {popupInfo && (
-          <Popup
-            latitude={popupInfo.lat}
-            longitude={popupInfo.lon}
-            onClose={() => setPopupInfo(null)}
-          >
-            <div>
-              <h3>{popupInfo.type}</h3>
-              <img src={popupInfo.imageURL} alt={popupInfo.type} className='w-32 h-auto' />
-              <p>{popupInfo.description}</p>
-            </div>
-          </Popup>
-        )}
-      </Map>
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
+
+  return (
+    <div className='flex h-full w-full'>
+      <div className='w-5% m-5'>
+        <select onChange={handleCityChange} value={selectedCity}>
+          <option value="">Select a city</option>
+          {egyptCities.map((city, index) => (
+            <option key={index} value={city}>{city}</option>
+          ))}
+        </select>
+      </div>
+      <div className='h-full w-full border border-black border-solid rounded-[25px]'>
+        <Map obstacles={obstacles} setPopupInfo={setPopupInfo} popupInfo={popupInfo} />
+      </div>
     </div>
   );
 };
