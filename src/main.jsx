d4fa5f8b352f7362cdcb6660effea2e0ae9019f3
Bichoo <bichoyatef0@gmail.com>
2024-02-24 14:17:29 +0200

    bichooo

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './styles/index.css';
-import Router from './router/router';
-import Layout from './components/Layout'; // Import the Layout component
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './styles/index.css'
+import Router from './router/router'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Layout>
+    <div className='flex w-screen h-screen'>
+      <Sidebar />
       <Router />
-    </Layout>
+    </div>
   </BrowserRouter>
-);
+)
